9ab8b2bd11a6c1373748c351ee78e0152a3f0464
carpediem-rtks <carpediem.rtks@gmail.com>
2022-03-08 11:07:39 +0900

    index.htmlのスタイルシートを作成しました。

---
+++ js/gamelist.js
@@ -0,0 +1,21 @@
+
+var games = [
+	{name:"KitPvP",ename:"kitpvp"},
+	{name:"鬼ごっこ倶楽部",ename:"OnigokkoClub"},
+	{name:"TableWars",ename:"tablewars"}
+];
+
+function writeGamesLinks(){
+	var ul = document.createElement("ul");
+	ul.className = "gameslinks";
+	for(var i = 0 ; i < games.length ; i++){
+		var l = document.createElement("li");
+		l.className = "gameslinks";
+		var agames = document.createElement("a");
+		agames.href = "https://carpediem-rtks.github.io/Carpediem/htmls/" + games[i].name + "/" + games[i].ename + ".html";
+		agames.innerHTML = games[i].name;
+		l.appendChild(agames);
+		ul.appendChild(l);
+	}
+	document.getElementById("games").appendChild(ul);
+}
